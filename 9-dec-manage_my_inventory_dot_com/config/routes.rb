51b2b8ccac169581cc5dba06081d5fc1e0684a25
Josh Cheek <josh.cheek@gmail.com>
2014-12-11 11:11:26 -0700

    Cleanup routes file

--- 9-dec-manage_my_inventory_dot_com/config/routes.rb
+++ 9-dec-manage_my_inventory_dot_com/config/routes.rb
@@ -1,64 +1,10 @@
 ManageMyInventoryDotCom::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   resources :merchants
-
   get '/search' => 'items#search', as: :search # search_path, search_url
-
   root to: 'merchants#index'
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id))(.:format)', constraints: Proc.new { |route_params, request|
+    # Record anything that hits the legacy route so that we know what we need to fix
     Rails.logger.info "\e[31mDEFAULT LEGACY ROUTE: #{request.method} #{request.path} | #{request.params.inspect}\e[0m"
     true
   }
