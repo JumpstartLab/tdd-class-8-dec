d897d2e71e13bca135ac527321208c1792f8583e
Josh Cheek <josh.cheek@gmail.com>
2014-12-12 14:19:35 -0700

    Update RSpec mocks example with the one we did together

--- 12-dec-rspec-mocks.rb
+++ 12-dec-rspec-mocks.rb
@@ -1,58 +1,72 @@
-# https://github.com/rspec/rspec-mocks
-
 class Book
-  def rspec_book
-    123
+  def pages
+    100
   end
 
-  def pages
-    456
+  def publish
+    true
   end
 end
 
-RSpec.describe 'Mock examples' do
+class Publisher
+  def initialize(book)
+    @book = book
+  end
+
+  def publish
+    @book.publish
+  end
+end
+
+RSpec.describe 'mocks and stubs' do
   example 'create an arbitrary object' do
-    user = double('user', name: 'Josh')
-    expect(user.name).to eq 'Josh'
+    book = double('book', publish: true)
+    expect(book.publish).to eq true
   end
 
   example 'dynamic return values' do
-    reverser = double('reverser')
-    allow(reverser).to receive(:reverse) { |reversable| reversable.reverse }
-    expect(reverser.reverse 'abc').to eq 'cba'
+    book = double('book')
+    allow(book).to receive(:publish) { |year| year > 1990 }
+    expect(book.publish 1991).to eq true
+    expect(book.publish 1990).to eq false
   end
 
   example 'stubbing existing methods' do
-    book = Book.new
-    allow(book).to receive(:pages).and_return(900)
-    expect(book.pages).to eq 900
+    expect(Book.new).to be_a_kind_of Book
+    allow(Book).to receive(:new).and_return(:not_a_book)
+    expect(Book.new).to eq :not_a_book
   end
 
   example 'stubbing when you don\'t have access to the instance' do
-    allow_any_instance_of(Book).to receive(:pages).and_return(100)
-    expect(Book.new.pages).to eq 100
+    allow_any_instance_of(Book).to receive(:publish).and_return(true)
+    expect(Book.new.publish).to eq true
   end
 
   example 'stubbing doesn\'t verify the invocation, use `expect` for that' do
-    book = Book.new
-    expect(book).to receive(:pages).and_return(900)
-    book.pages # this test would fail if we didn't call book.pages
+    book = double 'book'
+    expect(book).to receive(:publish)
+    Publisher.new(book).publish
+  end
+
+  example 'invocation matchers' do
+    book = double 'book'
+    expect(book).to receive(:publish).twice
+    book.publish
+    book.publish
   end
 
   describe 'verified mock' do
-    example 'you can override its methods with a stub' do
-      book = instance_double("Book", pages: 250)
-      expect(book.pages).to eq 250
-    end
-
-    example 'you can\'t call methods that aren\'t stubbed' do
-      book = instance_double("Book", pages: 250)
-      expect { book.rspec_book }.to raise_error
-    end
-
     example 'you can\'t stub methods that don\'t exist on the real object' do
-      expect { instance_double("Book", num_pages: 250) }
-        .to raise_error
+      book = instance_double Book, pages: 200 # if this was num_pages, it would fail
+      expect(book.pages).to eq 200
+    end
+
+    example 'you can\'t call methods that aren\'t stubbed, unless you declare it a "null object"' do
+      book = instance_double Book
+      expect { book.publish }.to raise_error # haven't stubbed publish
+
+      book.as_null_object
+      book.publish
     end
   end
 end
